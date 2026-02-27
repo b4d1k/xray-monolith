b20d7746d08e6dafb8b4fce8f7d1608e4a05e4df
B4D1K <b4d1k@ya.ru>
2026-02-28 01:30:10 +0300

    Fix missing game graph initialization for client scripts

--- src/xrGame/ai_space.cpp
+++ src/xrGame/ai_space.cpp
@@ -39,6 +39,7 @@ CAI_Space::CAI_Space()
 	m_script_engine = 0;
 	m_moving_objects = 0;
 	m_doors_manager = 0;
+	m_game_graph_owner = false;
 }
 
 void CAI_Space::init()
@@ -68,6 +69,20 @@ void CAI_Space::init()
 	m_script_engine = xr_new<CScriptEngine>();
 	script_engine().init();
 
+	string_path game_graph_file_name;
+	FS.update_path(game_graph_file_name, "$game_data$", "game.graph");
+	if (FS.exist(game_graph_file_name))
+	{
+		VERIFY(!m_game_graph);
+		IReader* stream = FS.r_open(game_graph_file_name);
+		if (stream)
+		{
+			m_game_graph = xr_new<CGameGraph>(*stream);
+			m_game_graph_owner = true;
+			FS.r_close(stream);
+		}
+	}
+
 #ifndef NO_SINGLE
 	extern string4096 g_ca_stdout;
 	setvbuf(stderr, g_ca_stdout,_IOFBF, sizeof(g_ca_stdout));
@@ -92,6 +107,10 @@ CAI_Space::~CAI_Space()
 	xr_delete(m_cover_manager);
 	xr_delete(m_graph_engine);
 	xr_delete(m_ef_storage);
+
+	if (m_game_graph_owner)
+		xr_delete(m_game_graph);
+	m_game_graph_owner = false;
 	VERIFY(!m_game_graph);
 }
 
@@ -214,6 +233,11 @@ void CAI_Space::patrol_path_storage(IReader& stream)
 void CAI_Space::set_alife(CALifeSimulator* alife_simulator)
 {
 	VERIFY((!m_alife_simulator && alife_simulator) || (m_alife_simulator && !alife_simulator));
+	if (alife_simulator && m_game_graph_owner)
+	{
+		xr_delete(m_game_graph);
+		m_game_graph_owner = false;
+	}
 	m_alife_simulator = alife_simulator;
 
 	VERIFY(!alife_simulator || !m_game_graph);
@@ -231,6 +255,7 @@ void CAI_Space::game_graph(CGameGraph* game_graph)
 	VERIFY(game_graph);
 	VERIFY(!m_game_graph);
 	m_game_graph = game_graph;
+	m_game_graph_owner = false;
 
 	//	VERIFY					(!m_graph_engine);
 	xr_delete(m_graph_engine);
