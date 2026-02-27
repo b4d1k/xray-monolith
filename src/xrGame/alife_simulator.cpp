cb92d026ea419eddde65e92f5835c2c248f7c241
B4D1K <b4d1k@ya.ru>
2026-02-27 19:09:35 +0300

    Sync actor id alias in host object map and initialize client ALife graph

--- src/xrGame/alife_simulator.cpp
+++ src/xrGame/alife_simulator.cpp
@@ -77,6 +77,18 @@ CALifeSimulator::CALifeSimulator(xrServer* server, shared_str* command_line) :
 	load(p.m_game_or_spawn, !xr_strcmp(p.m_new_or_load, "load") ? false : true, !xr_strcmp(p.m_new_or_load, "new"));
 }
 
+CALifeSimulator::CALifeSimulator(xrServer* server) :
+	CALifeUpdateManager(server, alife_section),
+	CALifeInteractionManager(server, alife_section),
+	CALifeSimulatorBase(server, alife_section)
+{
+	restart_all();
+	ai().set_alife(this);
+	reload(alife_section);
+	if (ai().get_game_graph())
+		graph().on_load();
+}
+
 CALifeSimulator::~CALifeSimulator()
 {
 	VERIFY(!ai().get_alife());
